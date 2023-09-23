044876ff78a5d39183c39ae29a8c34a13107f4d6
Anurag Gaiwal <anuraggaiwal0@gmail.com>
2023-09-23 13:36:31 +0530

    Weather app completed

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "e26a61fa4694483b8ca590c1aced04f1",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
